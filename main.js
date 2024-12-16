794fee616969bf26235766d0889ef7393fe49918
AbdulTv <menblogerman8@gmail.com>
2024-12-16 16:35:54 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function buttonYoriq() {
+
+let body = document.querySelector('body')
+body.style.backgroundColor = "Beige"
+
+}
+
+
+
+
+function buttonQorongi() {
+
+    let body = document.querySelector('body')
+    body.style.backgroundColor = "Black"
+    body.style.color = 'white'
+    
+    }
